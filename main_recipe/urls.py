362f3af66f91de61f6c8e5b79eb8ed416f05273c
Oleg Babenko <lokard.dev@gmail.com>
2020-12-21 22:09:02 +0200

    - templates home, category, about, constructor, base have been added - main_recipe/urls modified - main_recipe/views modified with CBV home, category, about, constructor

--- main_recipe/urls.py
+++ main_recipe/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', Index.as_view(), name='index')
+    path('home/', Home.as_view(), name='home'),
+    path('category/', Category.as_view(), name='category'),
+    path('constructor/', Constructor.as_view(), name='constructor'),
+    path('about/', About.as_view(), name='about')
 ]
--- main_recipe/views.py
+++ main_recipe/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.views import View
 
 
-class Index(View):
+class Home(View):
     def get(self, request):
-        return render(request, template_name='main_recipe/index.html')
+        return render(request, template_name='main_recipe/home.html')
+
+
+class Category(View):
+    def get(self, request):
+        return render(request, template_name='main_recipe/category.html')
+
+
+class Constructor(View):
+    def get(self, request):
+        return render(request, template_name='main_recipe/constructor.html')
+
+
+class About(View):
+    def get(self, request):
+        return render(request, template_name='main_recipe/about.html')
