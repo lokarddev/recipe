571ee38fae3a2c847ad4fb84b57cee4635c1c40c
Oleg Babenko <lokard.dev@gmail.com>
2020-12-21 20:55:08 +0200

    - base setting for main_recipe app

--- main_recipe/views.py
+++ main_recipe/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Index(View):
+    def get(self, request):
+        return render(request, template_name='main_recipe/index.html')
--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'recipe_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,8 +77,12 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'recipe',
+        'USER': 'postgres',
+        'PASSWORD': 'lok1415161213',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
