a59e25b0466d01fe976053cab9df24fea1fd0517
Oleg Babenko <lokard.dev@gmail.com>
2020-12-24 10:37:43 +0200

    - base models created

--- main_recipe/models.py
+++ main_recipe/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Copyright(models.Model):
+    """Information about copyrights in FOOTER of the site"""
+    copyright_title = models.CharField(max_length=150)
+    copyright_text = models.TextField()
+    copyright_link = models.CharField(max_length=100)
+
+
+class About(models.Model):
+    """Topic with content at page ABOUT"""
+    about_title = models.CharField(max_length=100)
+    about_text = models.TextField()
+
+
+class Product(models.Model):
+    """The name of ingredient used in a recipe"""
+    product_title = models.CharField(max_length=50)
+
+
+class Recipe(models.Model):
+    """Recipe itself"""
+    recipe_title = models.CharField(max_length=100)
+    recipe_text = models.TextField()
+    created = models.DateTimeField(auto_created=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+
+class Topic(models.Model):
+    """Random topic with content. Able to have "recipe model" info inside"""
+    topic_title = models.CharField(max_length=150)
+    topic_text = models.TextField()
+    created = models.DateTimeField(auto_created=True)
+    modified = models.DateTimeField(auto_now_add=True)
--- main_recipe/views.py
+++ main_recipe/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
 from django.views import View
+from models import *
 
 
 class Home(View):
     def get(self, request):
-        return render(request, template_name='main_recipe/home.html')
+        context = {
+
+        }
+        return render(request, template_name='main_recipe/home.html', context=context)
 
 
 class Category(View):
     def get(self, request):
-        return render(request, template_name='main_recipe/category.html')
+        context = {
+
+        }
+        return render(request, template_name='main_recipe/category.html', context=context)
 
 
 class Constructor(View):
     def get(self, request):
-        return render(request, template_name='main_recipe/constructor.html')
+        context = {
+
+        }
+        return render(request, template_name='main_recipe/constructor.html', context=context)
 
 
 class About(View):
     def get(self, request):
-        return render(request, template_name='main_recipe/about.html')
+        context = {
+
+        }
+        return render(request, template_name='main_recipe/about.html', context=context)
